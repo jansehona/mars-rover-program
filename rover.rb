f63f2142ecf584125f70d5b7fd02c42b742f41af
Jan Sehona <sehonajan@gmail.com>
2018-10-20 16:35:01 +0200

    updated rover.rb

--- rover.rb
+++ rover.rb
@@ -1,15 +1,6 @@
 require_relative "./direction"
 class Rover
-
-  # DIR = %w(N E S W).freeze
-  # attr_reader :face, :x_axis, :y_axis
-  # def initialize(max_x, max_y)
-  #   @x_axis = 0
-  #   @y_axis = 0
-  #   @max_x = max_x.to_i
-  #   @max_y = max_y.to_i
-  # end
-
+  
   attr_reader :direction, :position
 
   def initialize(initial_direction, current_position=[0,0])
